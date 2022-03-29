9cf807f8b0a0b6d59c4fca448e52430e0836dfe4
Yerkebulan Zhaiylkan <yerkkee@gmail.com>
2022-03-30 00:03:11 +0200

    Caesar encryption
    This is an application to encrypt text in Caesar encryption.

--- Ceaser.py
+++ Ceaser.py
@@ -1,3 +1,6 @@
+# Yerkebulan Zhaiylkan
+# This is an application to decrypt text in Caesar encryption.
+
 def caesar(plainText, shift): 
   cipherText = ""
   for ch in plainText:
@@ -10,38 +13,12 @@ def caesar(plainText, shift):
   print ("Your ciphertext is: ", cipherText)
   return cipherText
 #check the above function
-text = "cryptography"
-s = 22
+
+text = "cryptography" #Encrypted text
+s = 22 #Key of Plain Text
 
 print("Plain Text : " + text)
 print("Shift pattern : " + str(s))
 print("Cipher: " + caesar(text,s))
 
-
-# Function to find inverse permutations
-def inversePermutation(arr, size):
  
-    # Loop to select Elements one by one
-    for i in range(0, size):
- 
-        # Loop to print position of element
-        # where we find an element
-        for j in range(0, size):
- 
-        # checking the element in increasing order
-            if (arr[j] == i + 1):
- 
-                # print position of element where
-                # element is in inverse permutation
-                print(j + 1, end = " ")
-                break
- 
-# Driver Code
-arr = [6, 2, 4, 5, 7, 1, 8, 3]
-arr2 = ['b', 'h', 'd', 'f', 'a', 'c', 'g', 'e']
-size = len(arr)
-size2 = len(arr2)
-
-print(inversePermutation(arr2, size2))
- 
-#This code is contributed by Smitha Dinesh Semwal
